094e2f83eb8d9e0eca24da0d8b5f0275e5816a74
Otávio Pace <otavio.pace@pagar.me>
2019-01-15 22:41:07 -0200

    state: add NOT operator tests

--- src/state/tests.rs
+++ src/state/tests.rs
@@ -130,3 +130,22 @@ fn and() {
 
     compare_u8_slices(&new_state.output, &state.output);
 }
+
+#[test]
+fn not() {
+    let mut state = State::new([0; 255], [0; 255]);
+
+    state.ac = 10;
+
+    let new_state = (operator::NOT.run)(&state, 0);
+
+    assert_eq!(new_state.pc, state.pc + 1);
+
+    assert_eq!(new_state.ac, !state.ac);
+
+    assert_eq!(new_state.halt, false);
+
+    compare_u8_slices(&new_state.memory, &state.memory);
+
+    compare_u8_slices(&new_state.output, &state.output);
+}
