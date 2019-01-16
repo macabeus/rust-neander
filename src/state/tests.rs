aae75b8f62e752758a31cdfba5123ea7871d79f9
Otávio Pace <otavio.pace@pagar.me>
2019-01-15 22:40:49 -0200

    state: add AND operator tests

--- src/state/tests.rs
+++ src/state/tests.rs
@@ -109,3 +109,24 @@ fn or() {
 
     compare_u8_slices(&new_state.output, &state.output);
 }
+
+#[test]
+fn and() {
+    let mut state = State::new([0; 255], [0; 255]);
+
+    state.memory[50] = 20;
+
+    state.ac = 10;
+
+    let new_state = (operator::AND.run)(&state, 50);
+
+    assert_eq!(new_state.pc, state.pc + 2);
+
+    assert_eq!(new_state.ac, state.ac & 20);
+
+    assert_eq!(new_state.halt, false);
+
+    compare_u8_slices(&new_state.memory, &state.memory);
+
+    compare_u8_slices(&new_state.output, &state.output);
+}
